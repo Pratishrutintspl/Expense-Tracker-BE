7319079ee657a029cf1c0271dc85131fc9208e5a
Pratishrutintspl <pratishrutintspl@gmail.com>
2025-11-03 19:04:34 +0530

    register add

--- controller/userController.js
+++ controller/userController.js
@@ -39,5 +39,6 @@ const loginUser = async (req, res) => {
 
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require("express")
 const router = express.Router();
 const userController = require("../controller/userController")
 
-router.post("/register", userController.registerUser);
+router.post("/register", userController.registerUser);  
+// http://localhost:5000/api/auth/register ----  register User
 
 
-// router.post("/login", userController.loginUser);
+router.post("/login", userController.loginUser);
 
 
 // router.get("/profile", verifyToken, userController.getProfile);
